95775ee553509042a5956adb3199ac7058433754
Mike Covington <mfcovington@gmail.com>
2013-01-25 01:30:56 -0800

    cleanup

--- build_group_network.pl
+++ build_group_network.pl
@@ -10,25 +10,20 @@ use warnings;
 use autodie;
 use feature 'say';
 use File::Basename;
-use Data::Printer;
 
 my $genes_filename = $ARGV[0];
 open my $genes_fh, "<", $genes_filename;
-
 my %gene_groups;
 while (<$genes_fh>) {
-    chomp;
     my ( $read_id, $genes ) = split;
     $gene_groups{$genes}++;
 }
 close $genes_fh;
 
 my %cluster_sizes;
-# put the following into another loop to loop through each cluster
-while ( %gene_groups ) {
-    # my ( $seed_gene ) = split /\|/, ${ keys %gene_groups }[0];
-    my ( $group ) = keys %gene_groups;
-    my ( $seed_gene ) = split /\|/, $group;
+while (%gene_groups) {
+    my ($group) = keys %gene_groups;
+    my ($seed_gene) = split /\|/, $group;
     my %checked;
     my %cluster;
     while ( defined $seed_gene ) {
@@ -43,38 +38,6 @@ while ( %gene_groups ) {
         $seed_gene = shift @remainder;
     }
     $cluster_sizes{ scalar keys %cluster }++;
-    # p %cluster if scalar keys %cluster == 1; # some clusters have only one gene, because single reads map to multiple positions within the same gene
 }
 
-# p %gene_groups;
-# p %checked;
-# p %cluster;
-p %cluster_sizes;
-
-__END__
-some clusters have only one gene, because single reads map to multiple positions within the same gene
-    1    71,
-    2    1117,
-    3    234,
-    4    88,
-    5    40,
-    6    17,
-    7    22,
-    8    9,
-    9    7,
-    10   3,
-    11   2,
-    12   2,
-    13   1,
-    19   1,
-    20   3,
-    21   1,
-    26   1,
-    27   1,
-    29   1,
-    40   1,
-    41   1,
-    93   1
-
-
-
+exit;
