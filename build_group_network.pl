09395bb460a2414fb4c637d6b0ba05a869a16f22
Mike Covington <mfcovington@gmail.com>
2013-01-25 01:14:33 -0800

    identify all clusters

--- build_group_network.pl
+++ build_group_network.pl
@@ -12,8 +12,7 @@ use feature 'say';
 use File::Basename;
 use Data::Printer;
 
-my $seed_gene      = $ARGV[0];
-my $genes_filename = $ARGV[1];
+my $genes_filename = $ARGV[0];
 open my $genes_fh, "<", $genes_filename;
 
 my %gene_groups;
@@ -26,26 +25,56 @@ close $genes_fh;
 
 my %cluster_sizes;
 # put the following into another loop to loop through each cluster
-my %checked;
-my %cluster;
-while ( defined $seed_gene ) {
-    for my $genes ( keys %gene_groups ) {
-        next unless $genes =~ m|$seed_gene|;
-        my $counts = $gene_groups{$genes};
-        $cluster{$_} += $counts for split /\|/, $genes;
-        delete $gene_groups{$genes};
+while ( %gene_groups ) {
+    # my ( $seed_gene ) = split /\|/, ${ keys %gene_groups }[0];
+    my ( $group ) = keys %gene_groups;
+    my ( $seed_gene ) = split /\|/, $group;
+    my %checked;
+    my %cluster;
+    while ( defined $seed_gene ) {
+        for my $genes ( keys %gene_groups ) {
+            next unless $genes =~ m|$seed_gene|;
+            my $counts = $gene_groups{$genes};
+            $cluster{$_} += $counts for split /\|/, $genes;
+            delete $gene_groups{$genes};
+        }
+        $checked{$seed_gene}++;
+        my @remainder = grep { not $checked{$_} } keys %cluster;
+        $seed_gene = shift @remainder;
     }
-    $checked{$seed_gene}++;
-    my @remainder = grep { not $checked{$_} } keys %cluster;
-    $seed_gene = shift @remainder;
+    $cluster_sizes{ scalar keys %cluster }++;
+    # p %cluster if scalar keys %cluster == 1; # some clusters have only one gene, because single reads map to multiple positions within the same gene
 }
-$cluster_sizes{ scalar keys %cluster }++;
 
 # p %gene_groups;
 # p %checked;
-p %cluster;
+# p %cluster;
 p %cluster_sizes;
 
+__END__
+some clusters have only one gene, because single reads map to multiple positions within the same gene
+    1    71,
+    2    1117,
+    3    234,
+    4    88,
+    5    40,
+    6    17,
+    7    22,
+    8    9,
+    9    7,
+    10   3,
+    11   2,
+    12   2,
+    13   1,
+    19   1,
+    20   3,
+    21   1,
+    26   1,
+    27   1,
+    29   1,
+    40   1,
+    41   1,
+    93   1
 
 
 
