678a6886ae17d0f63337fed1a3d39f4c73f4f5ba
Mike Covington <mfcovington@gmail.com>
2013-02-02 19:52:24 -0800

    design hash of arrays of subclusters and add one cluster for testing

--- non-unique_length.pl
+++ non-unique_length.pl
@@ -15,6 +15,32 @@ use Number::Range;
 no warnings 'Number::Range';
 local $SIG{__WARN__} = sub { warn $_[0] unless $_[0] =~ m|Use of uninitialized value \$previous in string at .*Number/Range.pm line \d+.|};
 
+# For intial design and testing, will use:
+# CLUSTER 1001:
+# Solyc05g056050.2.1      3493
+# Solyc05g056060.2.1      9140
+# Solyc05g056070.2.1      9211
+# In the future; however, read in clusters from ID.clusters file and extract all appropriate subclusters from ID.groups file.
+# These are the subclusters for CLUSTER 1001:
+# 5718    Solyc05g056060.2.1|Solyc05g056070.2.1
+# 3422    Solyc05g056050.2.1|Solyc05g056060.2.1|Solyc05g056070.2.1
+# 71      Solyc05g056050.2.1|Solyc05g056070.2.1
+# Subclusters will be stored in HoA with cluster ID being the hash key and the value being an array of subclusters.
+
+my %clusters;
+my $cluster_id = 1001;
+my @subclusters = (
+    "Solyc05g056060.2.1|Solyc05g056070.2.1",
+    "Solyc05g056050.2.1|Solyc05g056060.2.1|Solyc05g056070.2.1",
+    "Solyc05g056050.2.1|Solyc05g056070.2.1"
+);
+$clusters{$cluster_id} = [@subclusters];
+use Data::Printer;
+p %clusters;
+
+say $clusters{$cluster_id};
+say $_ for split /\|/, $clusters{$cluster_id}->[0];
+say $_ for @{ $clusters{$cluster_id} };
 
 # for a cluster, read in seqreads. if a gene matches, consider read. increment uniq_count cluster, if applicable, else deal with multi_read
 # deal w/ multi_read = extract subcluster ID to use as hash key for hash of arrays (each element in array is read or at least relevant info of read)
@@ -60,10 +86,22 @@ $i++ for ($range->range);
 say $i;
 
 __END__
+{
+    1001   [
+        [0] "Solyc05g056060.2.1|Solyc05g056070.2.1",
+        [1] "Solyc05g056050.2.1|Solyc05g056060.2.1|Solyc05g056070.2.1",
+        [2] "Solyc05g056050.2.1|Solyc05g056070.2.1"
+    ]
+}
+ARRAY(0x7fab1b012ac8)
+Solyc05g056060.2.1
+Solyc05g056070.2.1
+Solyc05g056060.2.1|Solyc05g056070.2.1
+Solyc05g056050.2.1|Solyc05g056060.2.1|Solyc05g056070.2.1
+Solyc05g056050.2.1|Solyc05g056070.2.1
 
 300..350
 300..355
 300..355,450..600
 207
-
 
